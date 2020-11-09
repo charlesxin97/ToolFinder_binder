b49e7163f80ed8e5fc22e00ef1b9f3cd7eacd048
辛干 <charlesxin@shinkans-mbp.home>
2020-11-09 01:08:17 -0800

    Add pie chart

--- visualization/functional_classifier_module.py
+++ visualization/functional_classifier_module.py
@@ -5,6 +5,7 @@ from nltk.corpus import treebank
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import CountVectorizer
 from nltk.tokenize import word_tokenize
+
 nltk.download('punkt')
 nltk.download('treebank')
 nltk.download('stopwords')
@@ -21,24 +22,6 @@ import numpy as np
 import re
 import urllib
 from bs4 import BeautifulSoup
-
-
-url = input()
-response = urllib.request.urlopen(url)
-page = response.read()
-soup = BeautifulSoup(page, "lxml")
-# kill all script and style elements
-for script in soup(["script", "style"]):
-   script.extract()    # rip it out
-# get text
-text = soup.get_text()
-# break into lines and remove leading and trailing space on each
-lines = (line.strip() for line in text.splitlines())
-# break multi-headlines into a line each
-chunks = (phrase.strip() for line in lines for phrase in line.split("  "))
-# drp blank lines
-text = 'n'.join(chunk for chunk in chunks if chunk)
-input_text = text.split('.')
 
 
 class FFN(nn.Module):
@@ -66,3 +49,61 @@ class FFN(nn.Module):
 
 	def forward(self, inputs):
 		return self.model(inputs)
+
+
+def functional_predict(input_url='https://github.com/alvinzhou66/classification-of-scientific-software'):
+	url = input_url
+	response = urllib.request.urlopen(url)
+	page = response.read()
+	soup = BeautifulSoup(page, "lxml")
+	# kill all script and style elements
+	for script in soup(["script", "style"]):
+		script.extract()  # rip it out
+	# get text
+	text = soup.get_text()
+	# break into lines and remove leading and trailing space on each
+	lines = (line.strip() for line in text.splitlines())
+	# break multi-headlines into a line each
+	chunks = (phrase.strip() for line in lines for phrase in line.split("  "))
+	# drp blank lines
+	text = 'n'.join(chunk for chunk in chunks if chunk)
+	input_text = text.split('.')
+
+	vectorizer = joblib.load("../model/vectorizer.m")
+	input_sentences = ''
+	for line in input_text:
+		x_new_input = vectorizer.transform([line]).toarray()
+		model = FFN([1024, 2048, 1024, 512, 256], x_new_input.shape[1], 2)
+		model.load_state_dict(torch.load('../model/description.pt'))
+		model.eval()
+		test_pred = model(torch.tensor(x_new_input).float())
+		test_pred = torch.argmax(test_pred, dim=1, keepdim=False)
+		if test_pred.item() == 1:
+			input_sentences += str(line)
+
+	clf = joblib.load("../model/func_classifier.m")
+	x_new_input = vectorizer.transform([input_sentences]).toarray()
+	result = clf.predict(x_new_input)
+	rs1 = ''
+	if result == [0]:
+		print('This should be a DATA ANALYSIS project.')
+		rs1 = 'This should be a DATA ANALYSIS project.'
+	elif result == [1]:
+		print('This should be a DATA MANAGEMENT project.')
+		rs1 = 'This should be a DATA MANAGEMENT project.'
+	elif result == [2]:
+		print('This should be a DEEP LEARNING project.')
+		rs1 = 'This should be a DEEP LEARNING project.'
+
+	prob = clf.decision_function(x_new_input)[0]
+	new_prob = []
+	for x in prob:
+		x = float(x - np.min(prob)) / (np.max(prob) - np.min(prob))
+		new_prob.append(x)
+	summury = sum(new_prob)
+	new_prob = [x / summury for x in new_prob]
+	new_prob_dict = {'Data Analysis': new_prob[0], 'Deep Learning': new_prob[1], 'Data Management': new_prob[2]}
+	print('Chance to be DA:', new_prob[0])
+	print('Chance to be DM:', new_prob[1])
+	print('Chance to be DL:', new_prob[2])
+	return rs1, new_prob_dict
--- visualization/interactive_ui.py
+++ visualization/interactive_ui.py
@@ -2,7 +2,8 @@ import pandas as pd
 from bokeh.models import DatePicker
 from bokeh.layouts import column, row
 from bokeh.plotting import figure, curdoc
-from bokeh.models import Div, ColumnDataSource, HoverTool, SingleIntervalTicker, LinearAxis, Range1d, CustomJS, TextInput
+from bokeh.models import Div, ColumnDataSource, HoverTool, SingleIntervalTicker, LinearAxis, Range1d, CustomJS, \
+	TextInput, Button
 from bokeh.io import show
 from bokeh.palettes import GnBu3, OrRd3
 from bokeh.models.annotations import Title
@@ -28,6 +29,11 @@ import re
 import urllib
 from bs4 import BeautifulSoup
 from functional_classifier_module import *
+from math import pi
+from bokeh.io import output_file, show
+from bokeh.palettes import Category20c
+from bokeh.plotting import figure
+from bokeh.transform import cumsum
 
 title = Div(text="""<b>Classification of scientific software project</b><br>There are two parts in this 
 visualization page<br> (1) The functional classifier<br>(2) The visualization of the improvement of the other four 
@@ -38,17 +44,62 @@ a repository of a scientific software or the url for its readme file. Our model 
 and then use them to predict which type of the software it is as well as the corresponding possibility for 
 each type""", width=1000, height=100)
 
-text_input = TextInput(value="e.g. https://github.com/alvinzhou66/classification-of-scientific-software",
-                       title="Enter the project url for functional classifier here")
-text_input.on_change("value", lambda x, y, z: print(text_input.value))
+# x = {
+# 	'Data Analysis': 0.33,
+# 	'Deep Learning': 0.33,
+# 	'Data Management': 0.33
+# }
+
+source_pie = ColumnDataSource(data=dict(
+	start=[0, 0.33 * 2 * pi, 0.66 * 2 * pi], end=[0.33 * 2 * pi, 0.66 * 2 * pi, 2 * pi],
+	name=['Probability of Data Analysis', 'Probability of Deep Learning', 'Probability of Data '
+	                                                                      'Management'],
+	color=Category20c[3], value=[0.33, 0.33, 0.33]
+))
+
+p = figure(plot_height=350, title="Probability of being each functional type for the project (changes may take seconds"
+                                  ")", toolbar_location=None,
+           tools="hover", tooltips="@name: @value", x_range=(-0.5, 1.0))
+
+p.wedge(x=0, y=1, radius=0.4,
+        start_angle='start', end_angle='end',
+        line_color="white", fill_color='color', legend_field='name', source=source_pie)
+
+p.axis.axis_label = None
+p.axis.visible = False
+p.grid.grid_line_color = None
+
+software_type = ''
+prob_dict = {}
 
 
-def func_classifier_callback(attr, old, new):
-	print(text_input.value)
+def func_classifier_callback():
+	url = text_input.value
+	global software_type, prob_dict
+	software_type, prob_dict = functional_predict(url)
+	print(software_type, prob_dict)
+	new_dict = dict(name=['Probability of Data Analysis', 'Probability of Deep Learning', 'Probability of '
+	                                                                                      'Data '
+	                                                                                      'Management'],
+	                color=Category20c[3])
+	start = [0, prob_dict['Data Analysis'] * 2 * pi,
+	         prob_dict['Data Analysis'] * 2 * pi + prob_dict['Deep Learning'] * 2 * pi]
+	end = [prob_dict['Data Analysis'] * 2 * pi,
+	       prob_dict['Data Analysis'] * 2 * pi + prob_dict['Deep Learning'] * 2 * pi, 2 * pi]
+	value = [prob_dict['Data Analysis'], prob_dict['Deep Learning'], prob_dict['Data Management']]
+	new_dict['start'] = start
+	new_dict['end'] = end
+	new_dict['value'] = value
+	source_pie.data = new_dict
 
 
+button = Button(label="predict", button_type="success")
+text_input = TextInput(value="https://github.com/alvinzhou66/classification-of-scientific-software",
+                       title="Enter the project url for functional classifier here, for example:")
+
+button.on_click(func_classifier_callback)
 div_classifier_results = Div(text="""We also improve the four existing classifiers, we will show the statistics 
 accuracy, precision, recall and F_measure with respect to epochs. In addition we will provide the existing results 
 for comparison.""", width=1000, height=100)
 
-curdoc().add_root(column(title, div_functional_classifier, text_input, div_classifier_results))
+curdoc().add_root(column(title, div_functional_classifier, text_input, button, p, div_classifier_results))
